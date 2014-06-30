978a696b1a4fd14f5a13fad2144fe38825128f66
Malrok <themalrok@gmail.com>
2014-06-30 21:35:50 +0200

    Here we go

---
+++ desktop/src/com/mrk/goroda/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mrk.goroda.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mrk.goroda.GorodaMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GorodaMain(), config);
+	}
+}
